7ab0d573aabf9a1878e4b49b731ca6a2ff75bf70
Brad Schrag <brad.schrag@gmail.com>
2025-08-18 09:37:41 -0600

    default to true

--- lib/pp_sql.rb
+++ lib/pp_sql.rb
@@ -10,7 +10,7 @@ module PpSql
   end
   self.rewrite_to_sql_method = true
   self.add_rails_logger_formatting = true
-  self.disable_during_db_migrate = false
+  self.disable_during_db_migrate = true
 
   module Formatter
     private
